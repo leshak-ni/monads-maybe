ed9559553e76fd5f5d87e304cb3a347785a328ab
leshak-ni@users.noreply.github.com <git-pub@kornienko.org>
2015-05-01 12:03:47 +0300

    Add Test projects and first test

---
+++ Leshak.Monads.Maybe/MaybeExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leshak.Monads.Maybe
+{
+    public static class MaybeExtentions
+    {
+        public static TResult With<TInput, TResult>(this TInput Input, Func<TInput, TResult> evaluator)
+            where TInput : class
+            where TResult : class
+        {
+            return null;
+        }
+    }
+}
