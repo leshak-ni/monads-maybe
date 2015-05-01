4eddc4bbf5efda1ff142e21215aac77c5357fe5d
leshak-ni@users.noreply.github.com <git-pub@kornienko.org>
2015-05-01 19:26:05 +0300

    TS: switch to internal module style (tempoarary)

---
+++ Leshak.Monads.MaybeTS/file1.js
@@ -0,0 +1,9 @@
+﻿define(["require", "exports"], function(require, exports) {
+    var Maybe = (function () {
+        function Maybe() {
+        }
+        return Maybe;
+    })();
+    exports.Maybe = Maybe;
+});
+//# sourceMappingURL=file1.js.map
